1cf563a9187a7125fb2772e2ac11bc90e55d8eab
zaal6006 <alex.zaprometov@gmail.com>
2019-12-10 22:55:54 -0500

    added more styles css for miniMap

--- js/miniMap.js
+++ js/miniMap.js
@@ -68,10 +68,23 @@
                       resultStr += (key.toLowerCase() + value + '');
                     }
                     tabCellXId = 'y' + i + 'x' + j;
+                    // if ((i === (myArray.length -1)) && (j === (myArray[i].length -1))) {
+                    //     result += '<td class="miniMapTD" style="border: 4px solid #FF0000 " id="' + tabCellXId + '">'+
+                    //                 '<div class="divMinMapTD"><img src="./jpg/minimap/'+ resultStr +'.jpg" alt="[]" height="30" width="40"></div></td>';
+                    // } else {
+                    //     result += '<td class="miniMapTD" id="' + tabCellXId + '">'+
+                    //         '<div class="divMinMapTD" ' +
+                    //         ' style="background-image: url("""./jpg/minimap/'+ resultStr +'.jpg"""); ">' +
+                    //         '</div></td>';
+                    // }
+
                     if ((i === (myArray.length -1)) && (j === (myArray[i].length -1))) {
-                        result += '<td class="miniMapTD" style="border: 4px solid #FF0000 " id="' + tabCellXId + '"><img src="./jpg/minimap/'+ resultStr +'.jpg" alt="[]" height="30" width="40"></td>';
+                        result += '<td class="miniMapTD" style="border: 4px solid #FF0000 " id="' + tabCellXId + '">'+
+                            '<div class="divMinMapTD"><img src="./jpg/minimap/'+ resultStr +'.jpg" alt="[]" height="30" width="40"></div></td>';
                     } else {
-                        result += '<td class="miniMapTD" id="' + tabCellXId + '"><img src="./jpg/minimap/'+ resultStr +'.jpg" alt="[]" height="30" width="40"></td>';
+                        result += '<td class="miniMapTD" id="' + tabCellXId + '">'+
+                            '<div class="divMinMapTD" ' +
+                            ' style="background-image: "><img src="./jpg/minimap/'+ resultStr +'.jpg" alt="[]" height="30" width="40"></div></td>';
                     }
 
                     resultStr = "";
